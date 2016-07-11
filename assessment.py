af5bf4bd5245c43694778c77d6a52ddb48b81109
Cindy Panameno <CindyP@Cindys-MacBook-Pro.local>
2016-07-10 18:37:15 -0700

    Lists homework

---
+++ assessment.py
@@ -0,0 +1,163 @@
+"""List Assessment 
+
+Edit the functions until all of the doctests pass when
+you run this file.
+"""
+
+
+def all_odd(numbers):
+    """Return a list of only the odd numbers in the input list.
+
+    For example::
+
+        >>> all_odd([1, 2, 7, -5])
+        [1, 7, -5]
+
+        >>> all_odd([2, -6, 8])
+        []
+    """
+    # How is one odd? => is_odd(number)
+        # if its divisble by 2
+
+    # Returns True if NUMBER is divisible by 2
+    def is_divisible_by_2(number):
+        # divide number by 2 and remainder is 0 
+        if number % 2 == 0:
+            return True
+        return False
+
+
+    odd_list = []
+    for number in numbers:
+        if not is_divisible_by_2(number):
+           odd_list.append(number) #append numbers that returned true to new_list
+    
+    return odd_list
+
+
+def print_indices(items):
+    """Print index of each item in list, followed by item itself.
+
+    Do this without using a "counting variable" --- that is, don't
+    do something like this::
+
+        count = 0
+        for item in list:
+            print count
+            count = count + 1
+
+    Output should look like this::
+
+        >>> print_indices(["Toyota", "Jeep", "Volvo"])
+        0 Toyota
+        1 Jeep
+        2 Volvo
+    """
+
+    # Go through each item
+    # find the index
+    # return the item and its index
+    for i in range(0, len(items)):
+        print i, items[i]
+
+        
+
+def foods_in_common(foods1, foods2):
+    """Find foods in common.
+
+    Given 2 lists of foods, return the items that are in common
+    between the two, sorted alphabetically.
+
+    **NOTE**: for this problem, you're welcome to use any of the
+    Python data structures you've been introduced to (not just
+    lists). Is there another that would be a good idea?
+
+    For example::
+
+        >>> foods_in_common(
+        ...     ["cheese", "bagel", "cake", "kale"],
+        ...     ["hummus", "beets", "bagel", "lentils", "kale"]
+        ... )
+        ['bagel', 'kale']
+
+    If there are no foods in common, return an empty list::
+
+        >>> foods_in_common(
+        ...     ["lamb", "chili", "cheese"],
+        ...     ["cake", "ice cream"]
+        ... )
+        []
+
+    """
+    #turn foods1 and foods2 into sets
+    #compare set1 and set2 maybe using &
+    #add intersection to new list
+    foods_common = list(set(foods1) & set(foods2))
+    foods_common.sort
+    return foods_common
+    
+
+
+
+
+def every_other_item(items):
+    """Return every other item in `items`, starting at first item.
+
+    For example::
+
+       >>> every_other_item([1, 2, 3, 4, 5, 6])
+       [1, 3, 5]
+
+       >>> every_other_item(
+       ...   ["you", "z", "are", "z", "good", "z", "at", "x", "code"]
+       ... )
+       ['you', 'are', 'good', 'at', 'code']
+    """
+
+    for i in items:
+        return items[ : :2]
+
+
+def largest_n_items(items, n):
+    """Return the `n` largest integers in list, in ascending order.
+
+    You can assume that `n` will be less than the length of the list.
+
+    For example::
+
+        >>> largest_n_items([2, 6006, 700, 42, 6, 59], 3)
+        [59, 700, 6006]
+
+    It should work when `n` is 0::
+
+        >>> largest_n_items([3, 4, 5], 0)
+        []
+
+    If there are duplicates in the list, they should be counted
+    separately::
+
+        >>> largest_n_items([3, 3, 3, 2, 1], 2)
+        [3, 3]
+    """
+    # iterate through items 
+    # sort in ascending order 
+    # return items [len-n:len]
+
+    if n <= 0:
+        return []
+    items.sort()
+    return items[-n:]
+
+
+
+#####################################################################
+# END OF ASSESSMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
